11c4aa6c5a040a40faba2d11b508063316ef614e
corcheesey <micahcortese@gmail.com>
2025-04-06 20:07:53 -0400

    Deleted the JSON file and moved the data into the App.jsx. Added interactivity to the toolbar. Added functionality to the progress bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,104 @@
 import "./App.css";
-import data from "./flashcarddata.json";
 import { useState } from "react";
 
-function Flashcards() {
+/* Component Hierarchy
+
+  * App()
+    * FlashCardContainer()
+      * ProgressBar() 
+      * CardContent()
+      * ToolBar()
+
+*/
+
+// Contains the entire app
+export default function App() { 
+  return (
+    <>
+      <h1>Flash Cards</h1>
+      <FlashCardContainer carddata={CARDDATA} />
+    </>
+  );
+}
+
+// Displays the progress of the flash card deck
+function ProgressBar({ currentIndex, maxIndex}) {
+  const percentage = Math.round((currentIndex / maxIndex) * 100);
+
+  return (
+    <div
+      style={{
+        margin: "5px",
+      }}
+    >
+      <div
+        style={{
+          height: "40px",
+          width: "100%",
+          backgroundColor: "lightgrey",
+          borderRadius: "12px",
+        }}
+      >
+        <div
+          style={{
+            height: "100%",
+            width: `${percentage}%`,
+            backgroundColor: "grey",
+            textAlign: "right",
+            color: "white",
+            //paddingRight: "5px",
+            borderRadius: "12px",
+            transition: "width 0.3s ease"
+          }}
+        >
+          <div
+            style={{
+              position: "relative",
+              top: "7px",
+              right: "10px"
+            }}
+          >
+            {percentage}%
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function FlashCardContainer({ carddata }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isQuestion, setIsQuestion] = useState(true);
+  
+  const handleToggle = () => {
+    setIsQuestion(!isQuestion);
+  }
+
+  const nextCard = () => {
+    if (currentIndex < carddata.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      setIsQuestion(true);
+    }
+  }
+
+  const previousCard = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+      setIsQuestion(true);
+    }
+  }
+
+  return (
+    <div>
+      <ProgressBar currentIndex={currentIndex + 1} maxIndex={carddata.length} />
+      <CardContent card={carddata[currentIndex]} isQuestion={isQuestion} />
+      <ToolBar onToggle={handleToggle} onNext={nextCard} onPrevious={previousCard} />
+    </div>
+  )
+}
+
+// Displays the flash card data
+function CardContent({ card, isQuestion }) {
   return (
     <div
       style={{
@@ -16,54 +112,34 @@ function Flashcards() {
         margin: "5px"
       }}
     >
-      <p>{data[0].question}</p>
+      <p>{isQuestion ? card.question : card.answer}</p>
     </div>
   );
 }
 
-function Progressbar() {
-  return (
-    <div
-      style={{
-        textAlign: "left",
-        border: "2px solid grey",
-        borderRadius: "12px",
-        marginBottom: "5px",
-        paddingLeft: "10px",
-      }}
-    >
-      <p>0%</p>
-    </div>
-  );
-}
-
-function Toolbar() {
+// Receives the user input
+function ToolBar({ onToggle, onNext, onPrevious }) {
   return (
     <div>
       <div style={{ display: "flex", justifyContent: "space-between", backgroundColor: "grey", borderRadius: "12px", margin: "5px"}}>
         <div>
-          <button style={{background: "none"}}>{"<"} Previous</button>
+          <button style={{background: "none"}} onClick={onPrevious}>{"<"} Previous</button>
         </div>
         <div>
-          <button style={{background: "none"}}>Show Answer</button>
+          <button style={{background: "none"}} onClick={onToggle}>Show Answer</button>
         </div>
         <div>
-          <button style={{background: "none"}}>Next {">"}</button>
+          <button style={{background: "none"}} onClick={onNext}>Next {">"}</button>
         </div>
       </div>
     </div>
   );
 }
 
-export default function App() {
-  return (
-    <>
-      <h1>Flash Cards</h1>
-      <Progressbar />
-      <div style={{border: "2px solid grey", borderRadius: "12px"}}>
-        <Flashcards />
-        <Toolbar />
-      </div>
-    </>
-  );
-}
+const CARDDATA = [
+  {id: 1, question: "What is React?", answer: "React is a JavaScript library for building user interfaces, primarily used for developing single-page applications."},
+  {id: 2, question: "What is JSX in React?", answer: "JSX (JavaScript XML) is a syntax extension for JavaScript that allows you to write HTML-like code within JavaScript."},
+  {id: 3, question: "What is a component in React?", answer: "A component in React is a reusable piece of UI that can be either a function or a class."},
+  {id: 4, question: "What is the difference between functional and class components?", answer: "Functional components are simple JavaScript functions that return JSX, while class components are ES6 classes that extend React.Component and can manage state."},
+  {id: 5, question: "What is state in React?", answer: "State is an object that holds data that may change over time, allowing components to be dynamic and interactive."}
+];
